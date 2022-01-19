63cfd1aede2eba45d4f0cdbd048bba0911632c28
Fan Yu <562937707@qq.com>
2022-01-19 16:41:15 +0800

    medium

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import requests
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 API_KEY = "cf348c19923690dbd343239d75fb8d4a"
-LAT = 30.6667
-LONG = 104.0667
+LAT = 5.14
+LONG = 147.55
 
-parameter = {
+weather_params = {
     "lat": LAT,
     "lon": LONG,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily"
 }
 
-response = requests.get(OWM_ENDPOINT, params=parameter)
-response.raise_for_status()
-data = response.json()
+will_rain = False
 
-hourly = data["hourly"]
+response = requests.get(OWM_ENDPOINT, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_hour_data = weather_data["hourly"]
+weather_data_12hour = weather_hour_data[:12]
+
+
+for hour_data in weather_data_12hour:
+    if hour_data["weather"][0]["id"] < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+
